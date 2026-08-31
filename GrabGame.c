02326ae42955fcf7422f3f7e8a36329c5a0848de
otzpt <mrjs10375@gmail.com>
2026-08-31 20:16:34 +0100

    refactured object generation; redid big part of the code and optimized it; more comments; this is getting somewhere

--- GrabGame.c
+++ GrabGame.c
@@ -1,5 +1,4 @@
 // KEEP UNUSED LIBRARIES FOR NOW THEY MIGHT BE NECESSARY
-#include<stdio.h>
 #include<ncurses.h>
 #include<time.h>
 #include<stdlib.h>
@@ -9,9 +8,36 @@
 #define MAX_X (GAME_WIDTH - 4 - 1)
 #define GAME_WIDTH 30
 #define GAME_HEIGHT 31
-#define MAX_OBJECT 10
+#define MAX_OBJECT 30
+
+int PlatColision(int object_x[], int object_y[], int platform_x, int platform_y, int j);
 
 // functions
+void test_ground(int object_y[], int object_x[], int start_y, int start_x, int platform_x, int platform_y, int *objectGen)
+{
+    // this function generates the object '@'
+    // and keeps track of how many objects are on screen
+    // to avoid overflows;
+    // every loop is one object
+    for(int i = 0; i < MAX_OBJECT; i++) {
+        // check for colison
+        if(PlatColision(object_x, object_y, platform_x, platform_y, i)) {
+            (*objectGen)--; // if theres a colision remove one to the tota ammount
+        }
+        // spawns object at a random delay
+        if (*objectGen < MAX_OBJECT) {
+            int delay = rand() % 201 + 100; // calculates a random delay between spawns
+            object_x[i] = rand() % (GAME_WIDTH - 2) + 1; // generates object at random x position
+            usleep(delay * 1000); // usleep is in micro seconds so *1000
+            // generates the object and aupdates the counter
+            mvaddch(start_y + object_y[i], start_x + object_x[i], '@');
+            (*objectGen)++;
+        } else {
+            ;
+        }
+    }
+}
+
 void PlatMove(int ch, int *x)
 {
     if (ch == KEY_RIGHT && *x < MAX_X) {
@@ -25,58 +51,82 @@ void PlatMove(int ch, int *x)
 
 void borders(int x, int y, int start_y, int start_x)
 {
-    for (int x = 0; x < GAME_WIDTH; x++) {
+    for (x = 0; x < GAME_WIDTH; x++) {
         mvaddch(start_y, start_x + x, '-'); // adds '-' at the top borders
         mvaddch(start_y + GAME_HEIGHT - 1, start_x + x, '-'); // adds '-' at bottom borders
     }
-    for (int y = 0; y < GAME_HEIGHT; y++) {
+    for (y = 0; y < GAME_HEIGHT; y++) {
         // adds '|' to the sides
         mvaddch(start_y + y, start_x, '|');
         mvaddch(start_y + y, start_x + GAME_WIDTH - 1, '|');
     }
 }
 
-void Object_reset(int *y)
+void Object_reset(int object_x[], int object_y[], int platform_x, int platform_y, int i) // dont be tricked by this function names it does more than the name sugests
 {
-    (*y)++;
+    (object_y[i])++; // moves thee object down
     // resets to top
-    if (*y >= GAME_HEIGHT) {
-        *y = 1;
+    if (PlatColision(object_x, object_y, platform_x, platform_y, i)) {
+        object_y[i] = 1;
+    }
+    if (object_y[i] >= GAME_HEIGHT) {
+        object_y[i] = 1;
     }
 }
 
-void MultipleObjects(int y, int object_x[], int object_y[], int start_y, int start_x, int *objectC)
+void Object_genaration(int object_y[], int object_x[], int start_y, int start_x, int platform_x, int platform_y, int *objectGen)
 {
-    for (int i = 0; i <= MAX_OBJECT - 1; i++) {
-        object_x[i] = rand() % (GAME_WIDTH - 2) + 1; // randomizes @ position
-        object_y[i] = 1 + (y * 2);
-        mvaddch(start_y + object_y[i], start_x + object_x[i], '@'); // draws the falling object
-        // counts how many objects on screen
-        *objectC = *objectC + 1;
+    // this function generates the object '@'
+    // and keeps track of how many objects are on screen
+    // to avoid overflows;
+    // every loop is one object
+    for(int i = 0; i < MAX_OBJECT; i++) {
+        // check for colison
+        if(PlatColision(object_x, object_y, platform_x, platform_y, i)) {
+            (*objectGen)--; // if theres a colision remove one to the tota ammount
+        }
+        // spawns object at a random delay
+        if (*objectGen < MAX_OBJECT) {
+            int delay = rand() % 201 + 100; // calculates a random delay between spawns
+            object_x[i] = rand() % (GAME_WIDTH - 2) + 1; // generates object at random x position
+            usleep(delay * 1000); // usleep is in micro seconds so *1000
+            // generates the object and aupdates the counter
+            mvaddch(start_y + object_y[i], start_x + object_x[i], '@');
+            (*objectGen)++;
+        } else {
+            ;
+        }
     }
 }
 
-int PlatColision(int object_x[], int object_y[], int platform_x, int platform_y)
+int PlatColision(int object_x[], int object_y[], int platform_x, int platform_y, int j)
 {
-    if (object_y == platform_y && object_x >= platform_x && object_x < platform_x + 4) {
+    if (object_y[j] == platform_y && object_x[j] >= platform_x && object_x[j] < platform_x + 4) {
         return 1;
     }
     return 0;
 }
 
 int main(void) {
+    int objectGen = 0;
+    int j;
+    int i;
     int x = (GAME_WIDTH - 4) / 2;
     int running = 1;
-    int object_x[MAX_OBJECT], object_y[MAX_OBJECT], object_timer = 0, objectC = 1; // objectC is short for object counter, counts how many @ are in screnn there is a cap of 10 @
+    int object_x[MAX_OBJECT], object_y[MAX_OBJECT], object_timer = 0; // objectC is short for object counter, counts how many @ are in screnn there is a cap of 10 @
 
     srand(time(NULL));
+
+    for (int k = 0; k < MAX_OBJECT; k++) {
+        object_y[k] = 1;
+    }
 
     initscr();// init terminal src
     curs_set(0);// hides cursor
 
     int rows, cols;
     getmaxyx(stdscr, rows, cols);
-    int y = GAME_HEIGHT - 2; // puts platform on MENU BOTTOM bottom
+    int y = GAME_HEIGHT - 2; // puts platform on MENU bottom
 
     // centers the game
     int start_x = (cols - GAME_WIDTH) / 2;
@@ -91,11 +141,14 @@ int main(void) {
         int ch = getch();
 
         PlatMove(ch, &x);
-        if (PlatColision(object_x, object_y, x, y)) {
-            object_y[] = 1; // resets @ to the top if colision is detected with platform
-            //object_x[] = rand() % (GAME_WIDTH - 2) + 1; // randomizes @ position
+        // colision
+        for(j = 0; j <= MAX_OBJECT - 1; j++) {
+            if (PlatColision(object_x, object_y, x, y, j)) {
+                object_y[j] = 1; // resets @ to the top if colision is detected with platform
+            }
         }
 
+        // to close the game only when user presses 'q'
         if (ch == 'q') {
             running = 0;
         }
@@ -108,20 +161,17 @@ int main(void) {
 
         object_timer++;
 
-        int i;
         // faling object speed
         if (object_timer >= 20) {
             for (i = 0; i <= MAX_OBJECT - 1; i++) {
-                Object_reset(&object_y[i]);
+                Object_reset(object_x, object_y, x, y, i);
             }
             object_timer = 0;
         }
 
-        usleep(10000);
+        Object_genaration(object_y, object_x, start_y, start_x, x, y, &objectGen);
 
-        if (objectC == MAX_OBJECT) {
-            objectC = 1;
-        }
+        usleep(10000);
     }
     endwin();
     return 0;
